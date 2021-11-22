0b6a6e0120bc7ec3e823629186276cc671a0b6d3
ojfernandez <ojfer@uw.edu>
2021-11-22 03:43:29 -0800

    Removed build/ and config.json from .gitignore

---
+++ build/CMakeFiles/UWCrowBot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UWCrowBot.dir/src/main.cpp.o"
+  "UWCrowBot"
+  "UWCrowBot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UWCrowBot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
