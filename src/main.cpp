f197f4c592c5e4edac35c9a7ea67bb06daaeb6b5
Oliver Jeremiah Fernandez <ojfer@uw.edu>
2021-11-21 18:58:37 -0800

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,43 +6,34 @@
 
 using json = nlohmann::json;
 
-int main(int argc, char const *argv[])
-{
-    json configdocument;
-    std::ifstream configfile("../config.json");
-    configfile >> configdocument;
+int main(int argc, char const *argv[]) {
+   json configdocument;
+   std::ifstream configfile("../config.json");
+   configfile >> configdocument;
 
-    /* Setup the bot */
-    dpp::cluster bot(configdocument["token"]);
+   /* Setup the bot */
+   dpp::cluster bot(configdocument["token"]);
 
-    /* Log event */
-    bot.on_log([&bot](const dpp::log_t &event) {
-	if (event.severity >= dpp::ll_debug)
-	{
-		std::cout << dpp::utility::current_date_time() << " [" << dpp::utility::loglevel(event.severity) << "] " << event.message << "\n";
-	}
-    });
+   /* Log event */
+   bot.on_log([&bot](const dpp::log_t &event) {
+      if (event.severity >= dpp::ll_debug) {
+         std::cout << dpp::utility::current_date_time() << " [" << dpp::utility::loglevel(event.severity) << "] " << event.message << "\n";
+      }
+   });
 
-    /* Use the on_message_create event to look for commands */
-    bot.on_message_create([&bot](const dpp::message_create_t &event) {
+   /* Use the on_message_create event to look for commands */
+   bot.on_message_create([&bot](const dpp::message_create_t &event) {
 
-	std::stringstream ss(event.msg->content);
-	std::string command;
-	ss >> command;
+      std::stringstream ss(event.msg->content);
+      std::string command;
+      ss >> command;
 
-	if (command == "!hello")
-	{
-		bot.message_create(dpp::message(event.msg->channel_id, "Hello to you too."));
-	}
-	else if (command == "!ping")
-	{
-		bot.message_create(dpp::message(event.msg->channel_id, "Pong!"));
-	}
+      if (command == "!ping") {
+         bot.message_create(dpp::message(event.msg->channel_id, "Pong!"));
+      }
+   });
 
-    });
-
-    /* Start bot */
-    bot.start(false);
-    return 0;
+   /* Start bot */
+   bot.start(false);
+   return 0;
 }
-
