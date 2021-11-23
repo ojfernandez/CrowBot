ec19eeab92f149d8164174d0fe96ad20a48b584e
andororo <andrewpham_19@outlook.com>
2021-11-22 18:51:26 -0800

    Implemented crow facts and inspire. Update campus image pull

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,21 @@ int main(int argc, char const *argv[]) {
    json configdocument;
    ifstream configfile("../config.json");
    configfile >> configdocument;
+   ifstream infile("songs.txt");
+   bool songs = true;
+   bool crowFacts = true;
+   bool clubs = true;
+   if (!infile) {
+   	song = false;
+	cout << "Can't read song file" << endl;
+   }
 
    /* Setup the bot */
    dpp::cluster bot(configdocument["token"]);
+
+	//create vector of song
+	vector<string[3]> songSuggestions;
+	
 
    /* Log event */
    bot.on_log([&bot](const dpp::log_t &event) {
@@ -33,6 +45,25 @@ int main(int argc, char const *argv[]) {
       if (command == "!ping") {
          bot.message_create(dpp::message(event.msg->channel_id, "Pong!"));
       }
+
+      //!inspire
+      // A test command that returns a single insirpartional quote
+      if (command == "!inspire") {
+            
+            bot.message_create(dpp::message(event.msg->channel_id, "***SURPASS YOUR LIMITS***"));
+      }
+      
+      if (command == "!crowFact") {
+         vector<string> crowFacts = {"Young crows leave the nest after four weeks.", "Crows have around 250 different calls.", 
+            "Crow eggs hatch after 20-40 days.", "Crows are COOL", "Crows are part of the family Corvidae.", "Crows are omnivores."};
+         int max; 
+         max = 5;
+         srand(time(0));
+         bot.message_create(dpp::message(event.msg->channel_id, crowFacts.at(rand()%max)));
+      }
+
+	if (command == ("!suggestSongs") {
+	}
       
       // !campus
       // Sends an embedded image of the UWB campus grounds
