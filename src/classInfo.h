e1fcf4ec1a58e66df295deb7f9383c4a2e05a651
mendomic <michaelpatmendoza@gmail.com>
2021-12-06 13:00:12 -0800

    Create header

---
+++ src/classInfo.h
@@ -0,0 +1,13 @@
+#ifndef CLASSINFO_H
+#define CLASSINFO_H
+
+#include <dpp/dpp.h>
+#include <dpp/nlohmann/json.hpp>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include "cpr/cpr.h"
+
+std::string classInfo(std::string courseID);
+
+#endif
