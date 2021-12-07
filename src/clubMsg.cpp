85e713db8a308d2f6b11c2b2ab92abead12cd351
ojfernandez <ojfer@uw.edu>
2021-12-06 17:11:12 -0800

    Fixed clubs! functionality

--- src/clubMsg.cpp
+++ src/clubMsg.cpp
@@ -6,9 +6,5 @@ using namespace std;
 
 string clubMsg(const dpp::select_click_t& e) {
 
-<<<<<<< HEAD
-   return "From " + e.custom_id + ": " + e.values[0];
-=======
    return e.values[0];
->>>>>>> clubAddition
 }
