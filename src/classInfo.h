ebda809d7bffee1eb90cd7e8b48fc4b85d2ede89
mendomic <michaelpatmendoza@gmail.com>
2021-12-08 19:45:31 -0800

    Change to embed

--- src/classInfo.h
+++ src/classInfo.h
@@ -2,12 +2,8 @@
 #define CLASSINFO_H
 
 #include <dpp/dpp.h>
-#include <dpp/nlohmann/json.hpp>
-#include <sstream>
 #include <iostream>
-#include <fstream>
-#include "cpr/cpr.h"
 
-std::string classInfo(std::string courseID);
+dpp::embed classInfo(std::string courseID);
 
 #endif
