e154bea08fabcadaf1ef0dbbdb40fb9eebd2f129
mendomic <michaelpatmendoza@gmail.com>
2021-12-08 19:17:27 -0800

    Define debug()

---
+++ src/debug.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "debug.h"
+
+using namespace std;
+
+void debug(vector<string>& commands) {
+	// Add commands here to be tested
+	commands.push_back("!ping");
+	commands.push_back("!inspire");
+	commands.push_back("!crowFact");
+	commands.push_back("!songSuggest");
+	commands.push_back("!clubs");
+	
+	commands.push_back("---------");
+	
+}
